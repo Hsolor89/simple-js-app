0543a155469a991aab220f78da0f428d4e2f107d
Harley Solorzano <Hsolor89@gmail.com>
2022-06-21 23:48:19 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// Testing out the single-comment feature using two forward slashes 
+alert('Hello world');
+/* Testing out the a multi-line comment using a forward slash and asterisk */   
+
+document.write(favoriteFood);
+let favoriteFood = 'lasagna';
+
